f6b105b626a3743b6cda66c18319e686d6b8dc93
fjk <1586237690@qq.com>
2023-09-21 15:20:45 +0800

    fix: 🐛 修复commit配置
    修复commit配置
    
    BREAKING CHANGE: 🧨 无

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,19 +1,8 @@
 module.exports = {
   extents: ['@commitlint/config-conventional'],
   rules: {
-    'body-leading-blank': [1, 'always'],
-    'footer-leading-blank': [1, 'always'],
-    'header-max-length': [2, 'always', 72],
-    'scope-case': [2, 'always', 'lower-case'],
-    'subject-case': [
-      2,
-      'never',
-      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
-    ],
-    'subject-empty': [2, 'never'],
-    'subject-full-stop': [2, 'never', '.'],
-    'type-case': [2, 'always', 'lower-case'],
-    'type-empty': [2, 'never'],
+    'body-leading-blank': [2, 'always'], // body换行
+    'header-max-length': [2, 'never', 80], // header 最长80
     'type-enum': [
       2,
       'always',
